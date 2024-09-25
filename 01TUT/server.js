32ee611415b7064666be7b2abdc5aade3b701928
CathyWong <1064348766@qq.com>
2024-09-25 22:20:23 +0800

    update: chapter1

--- 01TUT/server.js
+++ 01TUT/server.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+// console.log("hello world");
+// console.log(global);
+
+//some common core modules
+const os = require("os");
+const path = require("path");
+const math = require("./math");
+const { add, subtract, multiply, divide } = require("./math");
+
+// console.log(os);
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(path.parse(__filename));
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+console.log(math.add(2, 3));
+console.log(add(2, 3));
+console.log(subtract(2, 3));
+console.log(multiply(2, 3));
+console.log(divide(2, 3));
--- chapter1.md
+++ chapter1.md
@@ -2,9 +2,24 @@
 
 > Node.js是一个免费、开源、跨平台的 JavaScript 运行时环境，它让开发人员能够创建服务器、Web 应用、命令行工具和脚本。
 
-* node.js与普通js的区别
+* node.js与普通js的区别:
   * Node在服务器上运行，而不是在浏览器上（后端而不是前端）
   * 控制台(console)是终端窗口(terminal)
   * 全局对象而不是window对象
     > global object instead of window object
-  * 
+  * 拥有Common Core modules
+  * Node.js 同时支持 CommonJS 和 ES 模块系统（自 Node.js v12 起）而在浏览器中，我们开始看到 ES 模块标准正在实现。
+* 模块导入语法：
+
+    ```js
+    const os = require('os')
+    ```
+
+* 模块导出语法
+
+    ```js
+    const add = (a, b) => a + b;
+    module.exports = { add };
+    //the codes above equals to codes below
+    exports.add = (a, b) => a + b;
+    ```
